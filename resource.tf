3a6a7b77c6867d6956afad2a33413bc4c6da3995
ravinderrao6789@gmail.com <ravinderrao6789@gmail.com>
2025-07-16 09:31:39 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -1,4 +1,17 @@
 resource "azurerm_resource_group" "terra" {
   name     = "terra"
   location = "East US"
+} 
+resource "azurerm_storage_account" "saterra" {
+    name                     = "storagecn05"
+    resource_group_name      = azurerm_resource_group.storeterra.name
+    location                 = azurerm_resource_group.storeterra.location
+    account_tier             = "Standard"
+    account_replication_type = "LRS"
+}
+ 
+resource "azurerm_storage_container" "sacontainer" {
+    name                  = "terra-container"
+    storage_account_name  = azurerm_storage_account.saterra.name
+    container_access_type = "private"
 }
